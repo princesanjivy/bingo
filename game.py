44c68a0e42cab1b1ac98e73f057176168e272879
princesanjivy <sanjivy.android@gmail.com>
2024-06-13 02:06:18 +0530

    made changes to check bingo all are crossed

--- game.py
+++ game.py
@@ -2,7 +2,8 @@ from prettytable import PrettyTable
 import requests
 import json
 import random
-import os, time
+import os
+import time
 
 from colorama import init as colorama_init
 from colorama import Fore
@@ -10,13 +11,12 @@ from colorama import Style
 
 colorama_init()
 
-
 URL = "http://localhost:8000/"
 
 print("\tWelcome to Bingo")
 
-d=[]
-a=[]
+d = []
+a = []
 
 def generate_random_matrix(rows, cols):
     numbers = list(range(1, 51))
@@ -29,10 +29,10 @@ def generate_random_matrix(rows, cols):
         matrix.append(row)
     return matrix
 
-name = input("Enter you name: ")
-requests.post(URL+"addPlayer", data=json.dumps({"name":name}))
+name = input("Enter your name: ")
+requests.post(URL + "addPlayer", data=json.dumps({"name": name}))
 mode = input("Do you want to enter 25 numbers (1 - 50) or generate random (m/r): ")
-if mode == "r": # random mode
+if mode == "r":
     random_matrix = generate_random_matrix(5, 5)
     a = random_matrix
 elif mode == "m":
@@ -40,10 +40,10 @@ elif mode == "m":
         b = []
         for j in range(5):
             c = int(input("Enter a number between 1 and 50: "))
-            while (not (1 <= int(c) <= 50)) or (c in d):
+            while (not (1 <= c <= 50)) or (c in d):
                 if c in d:
                     print("Entered value already exists.")
-                elif not (1 <= int(c) <= 50):
+                elif not (1 <= c <= 50):
                     print("Entered value is not in the range 1 to 50.")
                 c = int(input("Enter a number between 1 and 50: "))
             d.append(c)
@@ -55,15 +55,14 @@ else:
 
 game = True
 while game:
-    pass
-    print("1. Start\n2. Show Players\n3. Ready\n4. Exit")
+    print("\n1. Start\n2. Show Players\n3. Ready\n4. Exit")
     op = int(input("Enter your option: "))
     if op == 2:
-        resp = requests.get(URL+"players")
+        resp = requests.get(URL + "players")
         p = json.loads(resp.text)
         print(p)
     if op == 1:
-        resp = requests.get(URL+"start")
+        resp = requests.get(URL + "start")
         p = json.loads(resp.text)
         if p.get("message") == "start":
             game = False
@@ -73,16 +72,15 @@ while game:
     if op == 4:
         exit()
     if op == 3:
-        resp = requests.post(URL+"ready", data=json.dumps({"name":name}))
+        resp = requests.post(URL + "ready", data=json.dumps({"name": name}))
         print(resp.text)
 
-
-
 def display_bingo():
-    table=PrettyTable()
-    table.field_names=["B","I","N","G","O"]
-    for i in range(5):
-        table.add_row(a[i],divider=True)
+    print()
+    table = PrettyTable()
+    table.field_names = ["B", "I", "N", "G", "O"]
+    for row in a:
+        table.add_row(row, divider=True)
     print(table)
 
 def get_index(num):
@@ -94,180 +92,67 @@ def get_index(num):
 
 def check_cross():
     d = []
-    d.append(a[0]) # row  1
-    d.append(a[1]) # row 2
-    d.append(a[2]) # row 3
-    d.append(a[3]) # row 4
-    d.append(a[4]) # row 5
-
-    t = []
     for i in range(5):
-        for j in range(5):
-            if j == 0:
-                t.append(a[i][j])
-    d.append(t) # col 1
+        d.append(a[i])  # rows
+        d.append([a[j][i] for j in range(5)])  # columns
 
-    t = []
-    for i in range(5):
-        for j in range(5):
-            if j == 1:
-                t.append(a[i][j])
-    d.append(t) # col 2
+    d.append([a[i][i] for i in range(5)])  # diagonal 1
+    d.append([a[i][4 - i] for i in range(5)])  # diagonal 2
 
-    t = []
-    for i in range(5):
-        for j in range(5):
-            if j == 2:
-                t.append(a[i][j])
-    d.append(t) # col 3
+    crossed = [all(isinstance(x, str) for x in row) for row in d]
 
-    t = []
-    for i in range(5):
-        for j in range(5):
-            if j == 3:
-                t.append(a[i][j])
-    d.append(t) # col 4
-
-    t = []
-    for i in range(5):
-        for j in range(5):
-            if j == 4:
-                t.append(a[i][j])
-    d.append(t) # col 5
-
-    t = []
-    for i in range(5):
-        for j in range(5):
-            if i == j:
-                t.append(a[i][j])
-    d.append(t) # diag 1
-
-    t = []
-    for i in range(5):
-        for j in range(5):
-            if i + j == 5-1:
-                t.append(a[i][j])
-    d.append(t) # diag 2
-
-    crossed = [0]*len(d)
-
-    # for index in range(len(d)):
-    #     t = 0
-    #     for num in d[index]:
-    #         if num.endswith("X"):
-    #             t += 1
-    #     if t == 5:
-    #         crossed[index] = 1
-
-    # print(crossed)
-    # print(crossed.count(1))
-    if crossed.count(1) == 5:
+    if crossed.count(True) >= 5:
         return "Over"
     return "InProgress"
-
-def get_num_from_server():
-    resp = requests.get(URL+"num")
-    num_data = json.loads(resp.text)
-    print(num_data)
-    m = num_data.get("message")
-    if "num" in m:
-        return "r", m.get("num")
-    
-    return "n", m
-
-# def strike(text):
-#     result = ""
-#     for c in text:
-#         result = result + c + "\u0336"
-#     return result
-
-# print("Player made ready")
-# ready_resp = requests.post(URL+"ready", json={"name": name})
-# print(ready_resp.text)
 
 while True:
     display_bingo()
 
-    ready_resp = requests.post(URL+"ready", json={"name": name})
-    print(ready_resp.text)
-
     while True:
         resp = requests.get(URL + "numbers")
-        print(resp.text)
         message = json.loads(resp.text).get("message")
         if isinstance(message, dict) and "numbers" in message:
             numberss = message.get("numbers")
             break
         else:
-            print("Not all players are ready.")
+            print("===Not all players are ready.===")
             time.sleep(1)
-
 
     print(numberss)
     print()
 
-    num =int(input("Enter a number to cross: "))
-    if num !=  numberss:
+    num = int(input("Enter a number to cross: "))
+    if num != numberss:
         print("Number not matched")
         time.sleep(1)
-        resp_new = requests.post(URL+"crossed", data=json.dumps({"name":name}))
-        print("Not Crossed but moving next")
-        print(resp_new.text)
-        # os.system("clear")
     else:
         i, j = get_index(num)
         if i == -1 or j == -1:
             print("Number not found!")
             time.sleep(1)
-            resp_new = requests.post(URL+"crossed", data=json.dumps({"name":name}))
-            print("Not Crossed but moving next")
-            print(resp_new.text)
         else:
-            # a[i][j] = strike(str(a[i][j]))
             a[i][j] = f"{Fore.GREEN}{a[i][j]}{Style.RESET_ALL}"
-            resp_new = requests.post(URL+"crossed", data=json.dumps({"name":name}))
-            print(resp_new.text)
-
 
         display_bingo()
 
-
-        resp_new = requests.post(URL+"crossed", data=json.dumps({"name":name}))
-        print("Not Crossed but moving next")
+        resp_new = requests.post(URL + "crossed", data=json.dumps({"name": name}))
+        print("\n===Made a move===")
         print(resp_new.text)
 
-        # get next number
-        # loop until all players made a turn
-        # show msg saying all player not made a turn
-
-        # resp_new = requests.post(URL+"next", data=json.dumps({"name":name}))
-        # print(resp_new.text)
-
-
-        # acknowledging server my turn is complete
-        # resp = requests.post(URL+"name",data=json.dumps({"name":name}))
-        # print(resp.text)
-
-        v= check_cross()
-        if v == "Over":
+        if check_cross() == "Over":
             print("GAME OVER!!!")
             break
-        print()
-        print("="*100)
-        print()
-        # os.system("clear")
-        
-        while True:
-            next_resp = requests.post(URL+"next")
-            next_message = json.loads(next_resp.text).get("message")
-            print(next_message)
+        print("\n" + "="*100 + "\n")
+        os.system("clear")
 
-            if next_message == "continue":
+        while True:
+            input("Press Enter to check for the next number...")
+            next_resp = requests.post(URL + "next", data=json.dumps({"name": name}))
+            status = json.loads(next_resp.text).get("message")
+            if status:
                 break
             else:
-                print(next_message)
-                time.sleep(1)
-
-
-
-        
+                print("Waiting for other players to make their move...")
+                ready_resp = requests.post(URL + "ready", json={"name": name})
+                print(ready_resp.text)      
+                # time.sleep(1)
--- server/main.py
+++ server/main.py
@@ -5,89 +5,100 @@ import random
 class Body(BaseModel):
     name: str
 
+# class PlayerStatus:
+#     ready: int
+#     has_played_once: bool 
+
 # 0 = not ready
 # 1 = ready
 # 2 = won
 
+
 class Bingo:
     def __init__(self):
         self.nums = [num for num in range(1, 51)]
-        # self.__nums_str__ = ""
         self.__players__ = {}
         self.started = False
         self.current_number = None
-    
+        self.all_played_once = False
+        self.played = {}
+
     def numbers(self):
         if not self.started:
             return 1, "not started"
+        
+        # if self.all_not_ready() and self.is_all_played():
+        if self.is_all_played():
+            self.all_played_once = True
+            self.current_number = None
+            # reset all played to 0
+            for p in self.played:
+                self.played[p] = 0
+            # reset all players to ready state
+            # for p in self.__players__:
+            #     self.__players__[p] = 1
         
         if len(self.nums) == 0:
             self.reset()
 
         if self.current_number is not None:
             return 0, self.current_number
-
-        n = random.choice(self.nums)
-        self.nums.remove(n)
-        self.current_number = n
-
-        # self.__nums_str__ += str(n) + ","
         
+        if self.all_played_once or self.current_number is None:
+            print("generating number")
+            n = random.choice(self.nums)
+            self.nums.remove(n)
+            self.current_number = n
+
         return 0, self.current_number
     
     def reset(self):
         self.nums = [num for num in range(1, 51)]
-        self.__nums_str__ = ""
+        self.__players__ = {}
+        self.started = False
+        self.current_number = None
+        self.all_played_once = False
 
     def add_player(self, name):
         if name in self.__players__:
             return 1, "player exists"
         self.__players__[name] = 0
-
+        self.played[name] = 0 # used for tracking 
+        #  when to change current number for all
         return 0, self.__players__
 
     def players(self):
         return self.__players__
-    
+        
     def can_start(self):
-        for p in self.__players__:
-            if self.__players__[p] == 0:
-                return False
-            
-        return True
+        return all(status == 1 for status in self.__players__.values())
     
     def ready_player(self, name):
-        for p in self.__players__:
-            if p == name:
-                self.__players__[p] = 1 # ready
+        if name in self.__players__:
+            self.__players__[name] = 1
 
-    def un_ready_player(self, name):
-        for p in self.__players__:
-            if p == name:
-                self.__players__[p] = 0 # not ready
+    def unready_player(self, name):
+        if name in self.__players__:
+            self.__players__[name] = 0
 
-    
+    def game_played(self, name):
+        if name in self.played:
+            self.played[name] = 1
+
+    def is_all_played(self):
+        return all(status == 1 for status in self.played.values())
+
     def start(self):
         self.started = True
-    
-    def __len__(self):
-        print(self.__players__)
-        return len(self.__players__)
-    
 
-    def all_ready(self):
-        for p in self.__players__:
-            if self.__players__[p] != 1:
-                return False
-        return True
-
+    # def all_ready(self):
+    #     return all(status == 1 for status in self.__players__.values())
+        
     def all_not_ready(self):
-        for p in self.__players__:
-            if self.__players__[p] != 0:
-                return False
-        return True
-
-
+        return all(status == 0 for status in self.__players__.values())
+    
+    def get_status(self, name):
+        return self.__players__.get(name, None)
 
 app = FastAPI()
 bingo = Bingo()
@@ -98,72 +109,56 @@ def index():
 
 @app.get("/numbers")
 def number():
-    if not bingo.all_ready():
-        return {"message": "not all players are ready"}
-    
     s, info = bingo.numbers()
     if s != 0:
         return {"message": info}
-
     return {"message": {"numbers": info}}
+
+@app.get("/players")
+def players():
+    return {"message": {"players": bingo.players(), "played": bingo.played}}
+
+@app.get("/start")
+def start():
+    if not bingo.can_start():
+        return {"message": bingo.players()}
+    bingo.start()
+    return {"message": "start"}
+
+@app.get("/playerStatus")
+def player_status(name: str):
+    status = bingo.get_status(name)
+    if status is None:
+        return {"message": "player not found"}
+    return {"message": {"status": status, "name": name}}
+
+@app.post("/addPlayer")
+def add_player(body: Body):
+    status, plyr_info = bingo.add_player(body.name)
+    if status != 0:
+        return {"message": plyr_info}
+    return {"message": {"players": plyr_info}}
 
 @app.post("/newGame")
 def new_game():
     bingo.reset()
-    
     return {"message": "success"}
 
-@app.get("/players")
-def players():
-    info = bingo.players()
-    return {"message": {"players": info}}
-
-@app.post("/addPlayer")
-def add_player(body: Body):
-    status, info = bingo.add_player(body.name)
-    if status != 0:
-        return {"message": info}
-    
-    return {"message": {"players": info}}
-
-@app.get("/start")
-def start():
-    b = bingo.can_start()
-    print(b)
-    if not b:
-        return {"message": bingo.players()}
-
-
-    bingo.start()
-    return {"message": "start"}
-    
 @app.post("/ready")
 def ready(body: Body):
     bingo.ready_player(body.name)
-
     return {"message": "done"}
 
-
 @app.post("/next")
-def next():
-    print("status")
-    print(bingo.all_not_ready())
-    if  bingo.all_not_ready():
-        bingo.current_number = None
-        return {"message": "continue"}
-    
-    return {"message": "wait"}
+def next(body: Body):
+    return {"message": bingo.is_all_played() or bingo.all_played_once}
 
 @app.post("/crossed")
 def crossed(body: Body):
-    bingo.un_ready_player(body.name)
+    bingo.unready_player(body.name)
+    bingo.game_played(body.name)
 
     return {"message": "done"}
-
-
-
-
-
 
 
 
